d061c0b30d5350594732971218cead4d1a0211e6
rlnilsen <47765059+rlnilsen@users.noreply.github.com>
2020-10-14 16:54:06 +0200

    DAS Trainer: Add stats (part 1 of x) - Piece count - Percentage of pieces for each color (up to six colors selectable for stats) TODO: - Change stats indicators from tetriminos to colors - Per color profile stats color selection

--- simpledastrainer.s
+++ simpledastrainer.s
@@ -36,12 +36,33 @@
 ; replaces "jsr chooseNextTetrimino"
         jsr     setMissedEntryDelayTimer
 
-.segment "JMP_INIT_GAME_STATE"
-        ips_segment     "JMP_INIT_GAME_STATE",gameModeState_initGameState+9 ; $86E5
+.segment "DISABLE_PIECE_STATS"
+        ips_segment     "DISABLE_PIECE_STATS",incrementPieceStat ; $9969
 
-; at beginning of initGameState, replaces "ldx #$0F; lda #$00"
+; replaces "tax"
+       rts
+
+.segment "JMP_INIT_GAME_STATE"
+        ips_segment     "JMP_INIT_GAME_STATE",gameModeState_initGameState+21 ; $86F1, after statsByType init so we can overwrite
+
+; replaces "sta player1_tetriminoX; sta player2_tetriminoX"
         jsr initGameState_mod
         nop
+
+.segment "JMP_UPDATE_COLOR_STATS"
+        ips_segment     "JMP_UPDATE_COLOR_STATS",gameModeState_vblankThenRunState2 ; $9E27
+
+; replaces "lda #$02; sta gameModeState; jsr noop_disabledVramRowIncr"
+        jsr     updateAllStats
+        lda     #$02
+        sta     gameModeState
+        ; can drop jsr noop_disabledVramRowIncr because it is a noop
+
+.segment "JMP_CHECK_SKIP_RENDER_STATS"
+        ips_segment     "JMP_CHECK_SKIP_RENDER_STATS",$952A ; $952A
+
+; replaces "jsr copyPlayfieldRowToVRAM"
+        jsr     checkSkipRenderStats
 
 ; ----------------------------------------------------------------------------
 ; SWAP_TETRIMINO_TYPE
@@ -95,6 +116,135 @@ dasChargeColorSet2:
         .byte   $28,$28,$28,$28,$28,$28,$28,$28,$28,$28, $00,$00,$00,$00,$00,$00, $00 ; subset used if just missed entry delay
         .assert * - dasChargeColorSet2 = DASCHARGECOLORSETSIZE, error, "Color set has wrong size"
 
+statIndexToColor:
+        .byte   $ff, $16, $28, $10, $1c, $19, $ff
+
+; ----------------------------------------------------------------------------
+; 16-BIT DIVISION (16-BIT DIVIDEND, DIVISOR, RESULT AND REMAINDER)
+; https://codebase64.org/doku.php?id=base:16bit_division_16-bit_result
+; ----------------------------------------------------------------------------
+
+divisor = generalCounter        ;+1 used for hi-byte
+dividend = tmp1                 ;+1 used for hi-byte
+remainder = generalCounter3     ;+1 used for hi-byte
+result = dividend               ;save memory by reusing divident to store the result
+
+divide:
+	lda #0	        ;preset remainder to 0
+	sta remainder
+	sta remainder+1
+	ldx #16	        ;repeat for each bit: ...
+
+@divloop:
+	asl dividend	;dividend lb & hb*2, msb -> Carry
+	rol dividend+1
+	rol remainder	;remainder lb & hb * 2 + msb from carry
+	rol remainder+1
+	lda remainder
+	sec
+	sbc divisor	;substract divisor to see if it fits in
+	tay	        ;lb result -> Y, for we may need it later
+	lda remainder+1
+	sbc divisor+1
+	bcc @skip	;if carry=0 then divisor didn't fit in yet
+
+	sta remainder+1	;else save substraction result as new remainder,
+	sty remainder
+	inc result	;and INCrement result cause divisor fit in 1 times
+
+@skip:
+	dex
+	bne @divloop
+	rts
+
+
+; ----------------------------------------------------------------------------
+; FUNCTIONS COPIED FROM TAUS.S
+; ----------------------------------------------------------------------------
+
+; Convert 10 bit binary number (max 999) to bcd. Double dabble algorithm.
+; a:    (input) 2 high bits of binary number
+;       (output) low byte
+; tmp1: (input) 8 low bits of binary number
+; tmp2: (output) high byte
+binaryToBcd:
+        ldy     #00
+        sty     tmp2
+.if 1
+        ldy     #08
+.else
+        ; Uses 5 bytes to save 16 cycles
+        asl     tmp1
+        rol     a
+        rol     tmp2
+        ldy     #07
+.endif
+
+@while:
+        tax
+        and     #$0F
+        cmp     #$05
+        txa                     ; Does not change carry
+        bcc     @tensDigit
+        ; carry is set, so it will add +1
+        adc     #$02
+        tax
+@tensDigit:
+        cmp     #$50
+        bcc     @shift
+        clc
+        adc     #$30
+@shift:
+        asl     tmp1
+        rol     a
+        rol     tmp2
+        dey
+        bne     @while
+
+        rts
+
+; Multiply 16 bit number by 100
+; tmp1: (input)  LO
+;       (output) LO
+; tmp2: (input)  HI
+;       (output) HI
+multiplyBy100:
+        asl     tmp1    ; input =<< 2
+        rol     tmp2
+        asl     tmp1
+        rol     tmp2
+
+        lda     tmp1    ; output = input
+        ldx     tmp2
+
+        asl     tmp1    ; input =<< 3
+        rol     tmp2
+        asl     tmp1
+        rol     tmp2
+        asl     tmp1
+        rol     tmp2
+
+        clc             ; output += input
+        adc     tmp1
+        tay
+        txa
+        adc     tmp2
+        tax
+        tya
+
+        asl     tmp1    ; input =<< 1
+        rol     tmp2
+
+        clc             ; output += input
+        adc     tmp1
+        tay
+        txa
+        adc     tmp2
+
+        sty     tmp1
+        sta     tmp2
+        rts
+
 ; ----------------------------------------------------------------------------
 ; SET_BACKGROUND_COLOR_BY_DAS_CHARGE
 ; ----------------------------------------------------------------------------
@@ -102,18 +252,84 @@ dasChargeColorSet2:
 missedEntryDelayTimer := spawnCount+1 ; $001B
 missedEntryDelayButtonPressed := spawnCount+2 ; $001C
 
+STATSCOUNT = 7
+statsIncremented := spawnCount+3 ; $001D, STATSCOUNT bytes - set to 1 when index associated color detected, to avoid incrementing statsCounters more than once per piece
+statsCounters := $0780 ; STATSCOUNT*2 bytes - counts how many pieces has seen the index associated color
+
+; called for each new piece
+resetStatsIncremented:
+        lda     #0
+        ldx     #STATSCOUNT-1
+@loop:
+        sta     statsIncremented,x
+        dex
+        bpl     @loop
+        rts
+
+; called for each new game
+resetStatsCounters:
+        ; zero out all stats counters
+        lda     #0
+        ldx     #(STATSCOUNT*2)-1
+@loop:
+        sta     statsCounters,x
+        dex
+        bpl     @loop
+        ; except the first one which we set to 1
+        inc     statsCounters
+        ;jsr     updateAllStats not needed here because we do it every frame in patched gameModeState_vblankThenRunState2
+        rts
+
+; skip rendering of stats if copying playfield to vram, to avoid nmi taking longer time than vblank
+checkSkipRenderStats:
+        lda     vramRow
+        cmp     #$20
+        beq     @end
+        lda     outOfDateRenderFlags
+        and     #<~$40
+        sta     outOfDateRenderFlags
+@end:
+        jsr     copyPlayfieldRowToVRAM ; replaced code
+        rts
+
 initGameState_mod:
+        ; replaced code
+        sta     player1_tetriminoX
+        sta     player2_tetriminoX
+        ;
         lda     #0
         sta     missedEntryDelayTimer ; timer disabled for first piece
         sta     missedEntryDelayButtonPressed
-        ; replaced code
-        ldx     #$0F
-        lda     #$00
-        ;
+        jsr     resetStatsIncremented
+        jsr     resetStatsCounters
         rts
 
+; in: tmp1: color
+; out: x: index or $ff if not found, flags n+z set by x
+colorToStatIndex:
+        ldx     #6
+@loop:
+        lda     statIndexToColor,x
+        cmp     tmp1
+        beq     @ret
+        dex
+        bpl     @loop
+@ret:
+        rts
+
+; number of frames to check for missed left/right press after entry delay
+; in practise seems to be -3 off, since 9 gives two lines on level 18 (level 18 has 3 frames per line)
+MISSEDENTRYDELAYFRAMES = 9
+
+; called for each new piece except the first one
 setMissedEntryDelayTimer:
-        ldy     #9
+        jsr     resetStatsIncremented
+        ; inc 16 bits
+        inc     statsCounters
+        bne     :+
+        inc     statsCounters+1
+:
+        ldy     #MISSEDENTRYDELAYFRAMES
         sty     missedEntryDelayTimer
         ldy     #0
         sty     missedEntryDelayButtonPressed
@@ -191,9 +407,85 @@ renderDasCharge:
         tay
         ldx     dasChargeColorSet1,y
         
+        ; stats
+        stx     tmp3 ; save X
+        stx     tmp1
+        jsr     colorToStatIndex
+        bmi     @endStats ; color not found
+        lda     statsIncremented,x
+        bne     @endStats ; don't increase stat again untul new piece
+        lda     #1
+        sta     statsIncremented,x
+        ;
+        txa
+        asl     a
+        tax
+        ; inc 16 bits
+        inc     statsCounters,x
+        bne     :+
+        inc     statsCounters+1,x
+:
+@endStats:
+        ldx     tmp3 ; restore X
 
 @setColor:
         stx     PPUDATA ; replaced code
+        rts
+
+; calc percentages, conv to bcd, request transfer to ppu
+updateAllStats:
+        ldx     #(STATSCOUNT-1)*2
+@loop:
+        txa ; save X
+        pha ;
+        jsr     updateStat
+        pla ; restore X
+        tax ;
+        dex
+        dex
+        bpl     @loop
+        rts
+
+; in: x: index of stat * 2
+updateStat:
+        stx     generalCounter5 ; save X
+        cpx     #0
+        bne     @percent
+        ; convert to bcd
+        lda     statsCounters,x
+        sta     tmp1
+        lda     statsCounters+1,x
+        jsr     binaryToBcd
+        ldx     generalCounter5 ;restore X
+        sta     statsByType,x
+        lda     tmp2
+        sta     statsByType+1,x
+        jmp     @done
+@percent:
+        ; multiplyBy100
+        lda     statsCounters,x
+        sta     tmp1
+        lda     statsCounters+1,x
+        sta     tmp2
+        jsr     multiplyBy100
+        ; divide
+        lda     statsCounters
+        sta     divisor
+        lda     statsCounters+1
+        sta     divisor+1
+        jsr     divide
+        ; convert to bcd
+        ;result low byte is tmp1
+        lda     result+1
+        jsr     binaryToBcd
+        ldx     generalCounter5 ;restore X
+        sta     statsByType,x
+        lda     tmp2
+        sta     statsByType+1,x
+@done:
+        lda     outOfDateRenderFlags
+        ora     #$40
+        sta     outOfDateRenderFlags
         rts
 
 ; ----------------------------------------------------------------------------
